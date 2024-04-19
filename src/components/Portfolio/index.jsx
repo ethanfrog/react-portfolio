5e5e53acc531b02b9cf9944940037acddbf14a3e
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-04-19 19:55:49 -0400

    Added links to portfolio projects

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -8,68 +8,68 @@ function Portfolio() {
     {
       name: 'hangmanGame',
       description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://callen996.github.io/hangman-game/",
+      repo: "https://github.com/ethanfrog/hangman-game"
     },
     {
       name: 'realFakeStore',
       description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://boiling-harbor-15362-4fdcf0c8906a.herokuapp.com/",
+      repo: "https://github.com/ethanfrog/Fake-store"
     },
     {
       name: 'blockbuster2024',
       description: 'HTML/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://blockbuster2024.onrender.com",
+      repo: "https://github.com/ethanfrog/Blockbuster2024"
     },
     {
       name: 'passwordGenerator',
       description: 'Node/IoT',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://ethanfrog.github.io/password-generator/",
+      repo: "https://github.com/ethanfrog/password-generator"
     },
     {
       name: 'bootcampQuiz',
       description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://ethanfrog.github.io/bootcamp-quiz/",
+      repo: "https://github.com/ethanfrog/bootcamp-quiz"
     },
     {
       name: 'dailyPlanner',
       description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://ethanfrog.github.io/daily-planner/",
+      repo: "https://github.com/ethanfrog/daily-planner"
     },
     {
       name: 'forecastFinder',
       description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://ethanfrog.github.io/city-forecast-page/",
+      repo: "https://github.com/ethanfrog/city-forecast-page"
     },
     {
       name: 'textEditor',
       description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://pwa-text-editor-z23z.onrender.com/",
+      repo: "https://github.com/ethanfrog/pwa-text-editor"
     },
     {
       name: 'databaseManager',
       description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://github.com/ethanfrog/command-line-db-manager",
+      repo: "https://github.com/ethanfrog/command-line-db-manager"
     },
     {
       name: 'logoGenerator',
       description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://github.com/ethanfrog/logo-generator",
+      repo: "https://github.com/ethanfrog/logo-generator"
     },
     {
       name: 'commerceBackend',
       description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://github.com/ethanfrog/e-commerce-back-end",
+      repo: "https://github.com/ethanfrog/e-commerce-back-end"
     },
   ]);
 
