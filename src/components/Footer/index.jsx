69cd76f0629119a2193a9da18258323834c22f85
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-04-18 21:04:30 -0400

    Added links to GitHub and LinkedIn

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,16 +4,12 @@ function Footer() {
   const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/ethanfrog"
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/in/ethan-wynne-b2a956161/"
     },
-    {
-      name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
-    }
   ]
 
   return (
