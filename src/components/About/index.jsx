3c6299fde86960e90c3946bedccf20538f01b453
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-04-19 18:21:36 -0400

    Finished About page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,13 +3,14 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        {/*<i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>*/}
         </div>
         <p>
-        I'm a computer science graduate looking to enter a career.
+        Hi there! I'm a computer science graduate that's seeking an entry-level position in the field of web design.
+        I recently completed a web design boot camp at MSU to bolster my skill set in this area, and this portfolio serves as a demonstration of what I learned there.
         </p>
         <p>
-        Second paragraph.
+        Aside from programming, my other interests include typography, creative writing and graphic design.
         </p>
       </div>
     </section>
