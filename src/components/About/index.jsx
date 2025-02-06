ec986c7a1bd0a84760b0f0eb7eba9e4165e3c8ee
ethanfrog <ethan@phasic.com>
2025-02-05 22:20:00 -0500

    Update about section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,11 +6,11 @@ function About() {
         {/*<i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>*/}
         </div>
         <p>
-        Hi there! I'm a computer science graduate that's seeking an entry-level position in the field of web design.
-        I recently completed a web design boot camp at MSU to bolster my skill set in this area, and this portfolio serves as a demonstration of what I learned there.
+        Hi there! I'm a computer science graduate that's seeking an entry-level position in the field of software development.
+        I recently completed a web design boot camp at MSU to bolster my skill set, and this portfolio serves as a demonstration of what I learned there.
         </p>
         <p>
-        Aside from programming, my other interests include typography, creative writing and graphic design.
+        Aside from programming, my other interests include UI/UX design, graphic design and creative writing.
         </p>
       </div>
     </section>
